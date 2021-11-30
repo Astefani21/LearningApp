9f0e889ca09b359706cfe484b2272a02e7e7fbb3
Andy Stefani <astefani21@gmail.com>
2021-11-30 15:09:12 -0700

    Initial Commit

---
+++ DemoForFirebase/DemoForFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoForFirebaseApp.swift
+//  DemoForFirebase
+//
+//  Created by Alexis Willey on 11/30/21.
+//
+
+import SwiftUI
+
+@main
+struct DemoForFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
