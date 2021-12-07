5f93c5a9d8e75c2d247ae65ac9a67a775692b0d2
Andy Stefani <astefani21@gmail.com>
2021-12-07 16:18:54 -0700

    add asset add data files add models parsed json in content model parsed style data in content model

--- DemoForFirebase/ViewModels/ContentModel.swift
+++ DemoForFirebase/ViewModels/ContentModel.swift
@@ -10,4 +10,53 @@ import Foundation
 
 class ContentModel: ObservableObject {
     
+    //Whatever view code is using this published object it will update accordingly
+    @Published var modules = [Module]()
+    
+    var styleData: Data?
+    
+    init() {
+        getLocalData()
+    }
+    
+    func getLocalData() {
+        
+        // Get a url to the json file
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        // Read file into a data object
+        
+        do {
+            let jsonData = try Data(contentsOf: jsonUrl!)
+            
+            // Try to decode the json into an array of modules
+            let jsonDecoder = JSONDecoder()
+            
+            let modules = try jsonDecoder.decode([Module].self, from: jsonData)
+            
+            // Assign parsed modules to modules property
+            self.modules = modules
+        } catch {
+            
+            //TODO log error
+            print("Couldn't parse local data")
+            
+        }
+        
+        // Parse the style data
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+        
+        do {
+            
+            //Read the file into a data object
+            let styleData = try Data(contentsOf: styleUrl!)
+            
+            self.styleData = styleData
+            
+        } catch {
+            // Log error
+            print("Couldn't parse style data")
+        }
+        
+    }
 }
