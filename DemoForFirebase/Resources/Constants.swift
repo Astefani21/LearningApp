cb6e28615e69bd4a47babdcb8bd80aa974355d58
Andy Stefani <astefani21@gmail.com>
2021-12-09 15:34:38 -0700

    ContentDetailView with green Next Lesson button at bottom of video

---
+++ DemoForFirebase/Resources/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  DemoForFirebase
+//
+//  Created by Alexis Willey on 12/9/21.
+//
+
+import Foundation
+
+
+struct Constants {
+    
+    static var videoHostURl = "https://codewithchris.github.io/learningJSON/"
+}
--- DemoForFirebase/Views/ContentDetailView.swift
+++ DemoForFirebase/Views/ContentDetailView.swift
@@ -6,13 +6,58 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentDetailView: View {
     
     
+    
+    @EnvironmentObject var model: ContentModel
+    
     var body: some View {
         
+        let lesson = model.currentLesson
+        let url = URL(string: Constants.videoHostURl + (lesson?.video ?? ""))
         
+        VStack{
+            
+            if url != nil {
+                VideoPlayer(player: AVPlayer(url: url!))
+                    .cornerRadius(10)
+            }
+            
+            // Description
+            
+            // Show next lesson button only if there is a next lesson
+            if model.hasNextLesson(){
+                Button(action: {
+                    
+                    // Advnace the lesson
+                    model.nextLesson()
+                    
+                }, label: {
+                    
+                    ZStack{
+                        
+                        Rectangle()
+                            .frame(height:48)
+                            .foregroundColor(Color.green)
+                            .cornerRadius(10)
+                            .shadow(radius:5)
+                        
+                        Text("Next Lesson: \(model.currentModule!.content.lessons[model.currentLessonIndex + 1].title)")
+                            .foregroundColor(Color.white)
+                            .bold()
+                        
+                    }
+                    
+                    
+                })
+            }
+           
+        }
+        
+     
         
     }
 }
